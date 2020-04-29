6d7f5988bc3fcad065df6ed4c8451ceab2f392d8
sunnycarry <a0979478979@gmail.com>
2020-04-29 21:11:10 +0800

    add Music Scene

---
+++ XR_Project2/Assets/Scripts/Stage2/ButtonData.cs
@@ -0,0 +1,21 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class ButtonData : MonoBehaviour
+{
+    public float speed;
+    public float x_pos;
+    public float y_pos;
+
+    // Update is called once per frame
+    void Update()
+    {
+        
+    }
+
+    public void click()
+    {
+        //TODO judge 
+    }
+}
